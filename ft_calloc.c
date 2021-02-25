35afa2610358f06e09981c9293ea60f4c99d58e9
Ghosan <ahmednac.off@gmail.com>
2021-02-25 01:36:15 +0100

    test4

--- ft_calloc.c
+++ ft_calloc.c
@@ -18,6 +18,6 @@ void	*ft_calloc(size_t count, size_t size)
 
 	if (!(ptr = (void *)malloc(size * count)))
 		return (NULL);
-	ft_bzero(ptr, count);
+	ft_memset(ptr, 0, size*count);
 	return (ptr);
 }
